7e182294aef18fc822038a55c792bb7459931842
K Janakan <janakan86@gmail.com>
2019-05-02 06:03:21 +1000

    basics of binding

--- WeatherForecast/src/app/components/weather/weather.component.ts
+++ WeatherForecast/src/app/components/weather/weather.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
+
+  testValue : string = 'hello Interpolation';
+  imgLocation : string ='assets/intermittent.jpg';
+  twowayvalue : string;
+
+  testArrayValue : string[] = ['hello','to', 'Directives'];
+
+
+  showAlert(){
+    alert("Button clicked via Event binding !!!!");
+  }
 
   constructor() { }
 
