fce356525920c3076e51c51093fdeece4e91b025
Jana <janakan86@gmail.com>
2019-08-15 05:43:39 +1000

    Make images Stackblitz friendly

--- WeatherForecast/src/app/pipes/description-converter.pipe.ts
+++ WeatherForecast/src/app/pipes/description-converter.pipe.ts
@@ -7,6 +7,11 @@ export class DescriptionConverterPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
 
+    /* due to a limitation in stackblitz, the image paths are directly
+    linked to github URLs. Uncomment and use the following block if not
+    running via Stackblitz
+    https://github.com/stackblitz/core/issues/72
+    /*
     if (value == 'Sunny')
       return 'assets/sunny.jpg';
     else if (value == 'Partially sunny')
@@ -17,6 +22,19 @@ export class DescriptionConverterPipe implements PipeTransform {
         return 'assets/thunder.jpg';
     else if (value == 'Intermittent')
         return 'assets/intermittent.jpg';
+  */
+
+  if (value == 'Sunny')
+    return 'https://github.com/janakanag/AngularBasics/blob/master/WeatherForecast/src/assets/sunny.jpg?raw=true';
+  else if (value == 'Partially sunny')
+    return 'https://github.com/janakanag/AngularBasics/blob/master/WeatherForecast/src/assets/cloudy.jpg?raw=true';
+  else if (value == 'Rain')
+    return 'https://github.com/janakanag/AngularBasics/blob/master/WeatherForecast/src/assets/rain.jpg?raw=true';
+  else if (value == 'Thunder')
+    return 'https://github.com/janakanag/AngularBasics/blob/master/WeatherForecast/src/assets/thunder.jpg?raw=true';
+  else if (value == 'Intermittent')
+    return 'https://github.com/janakanag/AngularBasics/blob/master/WeatherForecast/src/assets/intermittent.jpg?raw=true';
+
 
     return null;
   }
